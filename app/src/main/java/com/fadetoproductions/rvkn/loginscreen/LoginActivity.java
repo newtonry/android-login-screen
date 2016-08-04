6789310733b075b35cf301dec484e4c8893c99ce
Ryan Newton <rnewton@hearsaycorp.com>
2016-08-03 19:59:00 -0700

    ...

---
+++ app/src/main/java/com/fadetoproductions/rvkn/loginscreen/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.fadetoproductions.rvkn.loginscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+//        getSupportActionBar().setLogo(getResources().getDrawable(R.drawable.ic_launcher));
+    }
+}
